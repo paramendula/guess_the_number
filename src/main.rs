10a947588b00a931bb49e46c7bb163c8771a470c
paramendula <paramendula@yandex.ru>
2022-09-23 17:17:02 +0000

    Version 0.1.0 initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use rand::Rng;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+    let mut trng = rand::thread_rng();
+
+    let mut tries = 0;
+    let mut secret_number: i32 = trng.gen_range(1..=100);
+
+    loop {
+        print!("Your guess: ");
+        
+        if let Err(_) = stdout.flush() {
+            break;
+        }
+
+        let mut guess = String::new();
+
+        if let Err(e) = stdin.read_line(&mut guess) {
+            println!("Couldn't read line: {e}");
+            break;
+        }
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(g) => g,
+            Err(_) => {
+                println!("Please, enter a valid number.");
+                continue;
+            }
+        }; 
+        
+        tries += 1;
+
+        if guess > secret_number {
+            println!("Too big!");
+        } else if guess < secret_number {
+            println!("Too small!");
+        } else {
+            println!("You won! Tries: {tries}. Press enter to play again.");
+            let mut temp = String::new();
+            if let Err(e) = stdin.read_line(&mut temp) {
+                println!("Couldn't read line: {e}");
+                break;
+            } else {
+                if !temp.trim().is_empty() {
+                    break;
+                }
+                tries = 0;
+                secret_number = trng.gen_range(1..=100);
+            }
+        }
+
+    }
 }
